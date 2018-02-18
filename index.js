ddb61b7afb7e47a1b384d5b4dac586a92c0c00ab
Peter Ayres <peter.ayresmusic@gmail.com>
2018-02-18 13:22:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // write your code below!
 var name = "Susan"
+<<<<<<< HEAD
 var height = 39
 var message = ` Susan is ${height} inches tall`
+=======
+var height = 74
+var message = ` is ${height} inches tall`
+>>>>>>> 5e38bd2a5abcce7d1ed7709bddacbcf8fe306aab
 
 module.exports = {name, height, message}
